ac8da18ef8c33dee0e887080307eb039c39d5910
Pierre Cordier <pierrot59430@hotmail.com>
2014-02-24 16:01:57 +0100

    pool.map sur ourMap
    J'ai changé la forme de ourMap, qui ne prend qu'un seul des éléments de
    la liste des chunk quand elle est appelée par pool.map.
    J'ai créé une fonction qui crée une liste avec pour chaque élément [un
    élément du chunk, les center]. ourMap prend en argument un des éléments
    de cette liste, et renvoit l'output que tu avais défini.

--- solution/main.py
+++ solution/main.py
@@ -31,131 +31,219 @@ import multiprocessing as mltp
 import fileManagement.function as fmf
 import KMeans.monoKMeans.function as monokmf
 import random as rd
-import KMeans.multiKMeans.function as multikmf
+#import KMeans.multiKMeans.function as multikmf
 
-## seed
-rd.seed(8)
-
-#####
-# input import
-#####
-
-# folder with the input file. we put it as working directory as we write the output in it.
-inOutFolder = input("Enter working directory (currently ../input/ please): ")
-print(inOutFolder)
-os.chdir(inOutFolder)
-
-# number of clusters and method
-k = input("Enter number of clusters (only positive integers please): ")
-k = int(k)
-method = input("Enter distribution method (mono or multi please): ")
-
-# building a dictionary x from csv file
-(x, varName, inFileRow) = fmf.featureArrayFromCsv("data.csv")
 
 
 #def function qui iront dans multiKmeans
 
-# pool.map prend en argument une fonction et une liste. Les Ã©lÃ©ments de la liste
-# sont callable par la fonction. pool est un objet constituÃ© de plusieurs processus.
-# map est une procÃ©dure callable sur les objets de ce type. Dans chaque processus de pool, 
-# map met en mÃ©moire la fonction, et un certain nombre dÃ©lÃ©ments de la liste. Chaque 
-# Ã©lÃ©mment soit rÃ©parti vers l'un des processus, sans rÃ©pÃ©tition et sans oubli. Dans
-# chaque processus, map demande l'appel de la fonction sur chaque Ã©lÃ©ment qui s'y trouve.
-# La sortie est la liste des rÃ©ponses de la fonction Ã  chaque Ã©lÃ©ment. Si la fonction
+# pool.map prend en argument une fonction et une liste. Les éléments de la liste
+# sont callable par la fonction. pool est un objet constitué de plusieurs processus.
+# map est une procédure callable sur les objets de ce type. Dans chaque processus de pool, 
+# map met en mémoire la fonction, et un certain nombre déléments de la liste; tel que chaque 
+# élémment soit réparti vers l'un des processus, sans répétition et sans oubli. Dans
+# chaque processus, map demande l'appel de la fonction sur chaque élément qui s'y trouve.
+# La sortie est la liste des réponses de la fonction à chaque élément. Si la fonction
 # renvoit un dictionnaire, la sortie est une liste de dictionnaire. La liste de sortie
-# est de la mÃªme taille que la liste d'entrÃ©e.
+# est de la même taille que la liste d'entrée.
 
+def chunk(x, m):
+        """
+        Returns a list() of m subsets of the array x by row
+        i.e. the subsets have the same number of columns.
+        """
+        n = x.shape[0]
+        quo, mod = divmod(n, m)
+        res = []
+        for i in range(0, m):
+                if i < m-1:
+                        res.append(x[(i*quo):((i+1)*quo),:])
+                else:
+                        res.append(x[(i*quo):((i+1)*quo + mod),:])
+        return(res)
+
+def listToMap(myChunk, myCenters):
+        """
+        Returns a list. Each element a piece of the chunk + centers.
+        """
+        res = []
+        for chunk in myChunk:
+                res.append([chunk, myCenters])
+        return(res)
+    
+    
+def newCenterMap(xMap, vecAllocMap, k):
+        """
+        Works on a CHUNKED input -> needs to be encapsuled in ourMap(L)
+        
+        Returns the LOCAL updates centers with regard to
+        the new allocation of x's arround the new centers.
+        WARNING : we don't take into account the case when
+        a center 'has no point around it'.
+        This function also returns the number of points
+        arround each local center as it is needed
+        to compute the final centers.
+        """
+        p = xMap.shape[1]
+        
+        kNew = np.unique(vecAllocMap).shape[0]
+        if kNew < k:
+                print("Warning: a center at least has no point around it.")
+
+        centerMap = np.ones((k, p))
+        nbMap = np.zeros((k,1))
+        for ic in range(0, k):
+                w = np.where(vecAllocMap[:,0] == ic)[0] # [0] because where returns a tuple (array, )
+                nbMap[ic,0] = int(w.shape[0])
+                if nbMap[ic,0] != 0:
+                        # else we let the initialzing "ones", as they will not be used since nbMap = 0
+                        centerMap[ic,:] = xMap[w,:].mean(axis = 0)
+        return(centerMap, nbMap)
+
+
+def ourMap(listChunk):
+        """
+        Function that maps what we want to all the
+        elements of a list. The list is typically
+        the list of chunks of our input data.
+
+        The (real global) centers are supposed to be known.
+        """
+        res = []
+        center = listChunk[1]
+        xMap = listChunk[0]
+        ### Computation of the distances between points and centers
+        matDistMap = monokmf.allDistance(xMap, center)
+        ### Computation of the local allocation
+        vecAllocMap = monokmf.alloc(matDistMap)
+        ### Computation of local centers and populations
+        (centerMap, nbMap) = newCenterMap(xMap, vecAllocMap, len(center))
+
+        res.append((centerMap, nbMap))
+        return(res)
+    
+if __name__ == "__main__":
+    ## seed
+    rd.seed(8)
+
+    #####
+    # input import
+    #####
+
+    # folder with the input file. we put it as working directory as we write the output in it.
+    inOutFolder = "../input/    "#input("Enter working directory (currently ../input/ please): ")
+    print(inOutFolder)
+    os.chdir(inOutFolder)
+
+    # number of clusters and method
+    global k
+    k = 3   #input("Enter number of clusters (only positive integers please): ")
+    k = int(k)
+    method = "multi" #input("Enter distribution method (mono or multi please): ")
+
+    # building a dictionary x from csv file
+    (x, varName, inFileRow) = fmf.featureArrayFromCsv("data.csv")
 
 #####
 # clustering mono thread
 #####
-if method == "mono":
-    start = time.time()
+    if method == "mono":
+        start = time.time()
 
-    center = monokmf.initialize(x, k)
-    hasConverged = False
+        center = monokmf.initialize(x, k)
+        hasConverged = False
 
-    while not hasConverged:
-        centerOld = center
-        matDist = monokmf.allDistance(x, center)
-        vecAlloc = monokmf.alloc(matDist)
-        center = monokmf.newCenter(x, vecAlloc, k)
-        hasConverged = (center == centerOld).all()
+        while not hasConverged:
+            centerOld = center
+            matDist = monokmf.allDistance(x, center)
+            vecAlloc = monokmf.alloc(matDist)
+            center = monokmf.newCenter(x, vecAlloc, k)
+            hasConverged = (center == centerOld).all()
 
-    end = time.time()
-    print("""Execution time of sequential K-means = """, end - start)
+        end = time.time()
+        print("""Execution time of sequential K-means = """, end - start)
 
+        #####
+        # output export (vecAlloc n'existe pas encore dans multi donc on peut pas tester si à la fin)
+        #####
+        # writing the result with a new column
+        xAugmented = np.hstack((x, vecAlloc)) # argument is a real tuple => (,) inside the ().
+        fmf.csvFromFeatureArrayAndClust("data_clustered_" + method + str(k) + ".csv",\
+                                varName,\
+                                xAugmented)
+
+    
+    
     #####
-    # output export (vecAlloc n'existe pas encore dans multi donc on peut pas tester si Ã  la fin)
+    # clustering multi thread
     #####
-    # writing the result with a new column
-    xAugmented = np.hstack((x, vecAlloc)) # argument is a real tuple => (,) inside the ().
-    fmf.csvFromFeatureArrayAndClust("data_clustered_" + method + str(k) + ".csv",\
-                               varName,\
-                               xAugmented)
+    elif method == "multi":
+        start = time.time()
 
+        # count number of cpu
+        m = mltp.cpu_count()
+        # lance autant de workers qu'il y a de cpu
+        pool = mltp.Pool(processes=m)
+        # Partitionne les données une fois multikmf.
+        chunkedX = chunk(x, m)
+        # Initialiser les centres globaux
+        center = monokmf.initialize(x, k)
+        #print(len(center))
+        # Créer la liste pour pool.map
+        xMapList = listToMap(chunkedX, center)
+        
+        # pool.map prend en argument une fonction et une liste
+        # Role ourMap, associer un centre à chaque point: centre le plus proche du point
+        # La liste est de longueur égale au nombre de process et contient les différentes
+        # parties des données de départ. Chaque élément de la liste inclut les centres actuels.
+        # Elle renvoit une liste avec chaque point, et le centre dont il est le plus proche
+        xMapListProcessed = pool.map(ourMap, xMapList)
+        print(xMapListProcessed)
+        # La fonction ourReduce prend en argument un dictionnaire, dont les clefs
+        # sont les centres, et les valeurs, les points associés + le nombre de points associés.
+        # Elle renvoit points et leur centre associé.
     
-    
-#####
-# clustering multi thread
-#####
-elif method == "multi":
-    
-    
-    start = time.time()
-
-    # count number of cpu
-    m = mltp.cpu_count()
-	# lance autant de workers qu'il y a de cpu
-    pool = mltp.Pool(processes=m)    
-    # Partitionne les donnÃ©es une fois
-    xMapList = multikmf.chunk(x, m)
-    # Initialiser les centres globaux
-    center = monokmf.initialize(x, k)
-    #hasConverged = False
-
     #while not hasConverged:
     #    centerOld = center
-    #    xMapListProcessed = pool.map(mutlikmf.ourMap, xMapList)
+    #xMapListProcessed = pool.map(mutlikmf.ourMap, xMapList)
     #    center = pool.map(multikmf.ourReduce, xMapListProcessed)
     #    hasConverged = (center == centerOld).all()
     
-    #####
-    # output export (vecAlloc n'existe pas encore dans multi donc on peut pas tester si Ã  la fin)
-    #####
-    # writing the result with a new column
-    #xAugmented = np.hstack((x, vecAllocMap)) # argument is a real tuple => (,) inside the ().
-    #fmf.csvFromFeatureArrayAndClust("data_clustered_" + method + str(k) + ".csv",\
-    #                           varName,\
-    #                           xAugmented)
+        #####
+        # output export (vecAlloc n'existe pas encore dans multi donc on peut pas tester si à la fin)
+        #####
+        # writing the result with a new column
+        #xAugmented = np.hstack((x, vecAllocMap)) # argument is a real tuple => (,) inside the ().
+        #fmf.csvFromFeatureArrayAndClust("data_clustered_" + method + str(k) + ".csv",\
+        #                           varName,\
+        #                           xAugmented)
 
     
-    end = time.time()
-    print("""Execution time of mutli-processed K-means = """, end - start)
+        end = time.time()
+        print("""Execution time of mutli-processed K-means = """, end - start)
 
-### Analytiquement, on peut diffÃ©rencier le temps de mise 
+### Analytiquement, on peut différencier le temps de mise 
     ### en place du threading, et le temps de calcul dans le
     ### threading
     ### Time1Thread: Tester le temps avec un thread mais 
     ### en passant par pool.map
     ### TimeSeq = Le comparer au temps sequentiel
-    ### TimeStruct = Time1Thread - TimeSeq Ã©value le temps
+    ### TimeStruct = Time1Thread - TimeSeq évalue le temps
     ### imposer par la mise en place de la structure de thread
     ### Time4Thread Tester le temps avec 4 thread
-    ### HypothÃ¨se: 4 Thread plus rapide que 1 Thread ?
+    ### Hypothèse: 4 Thread plus rapide que 1 Thread ?
     ### Time4Thread < Time1Thread ?
-    ### HypothÃ¨se: 4 Thread plus rapide que TimeSeq ?
+    ### Hypothèse: 4 Thread plus rapide que TimeSeq ?
     ### Time4Thread < TimeSeq ?
     ### prise en compte de timestruct si on se rend compte que
     ### finalement Time4Thread > TimeSeq
     ### Time4Thread - TimeStruct < Seq ?
-    ### Evaluer si timestruct est le mÃªme pour 1 ou 4 thread 
+    ### Evaluer si timestruct est le même pour 1 ou 4 thread 
     ### ou si 4 fois plus, ou autre
 
     
-else:
-    print("Error: Wrong method type provided.")
+    else:
+        print("Error: Wrong method type provided.")
 
           
 
